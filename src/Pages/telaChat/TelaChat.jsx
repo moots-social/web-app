ec0abea2a91bf2c7bd53d1f68eae682cdaba2a50
liviaBELLO <livia_bello@outlook.com>
2024-09-09 11:31:02 -0300

    pefil

--- src/Pages/telaChat/TelaChat.jsx
+++ src/Pages/telaChat/TelaChat.jsx
@@ -1,6 +1,7 @@
 import "./telaChat.css";
 import SideBar from "../../Components/SideBar/SideBar";
 import imagemPerfil from "../../assets/img/imagemPerfil.png";
+import gabriel from "../../assets/img/chatGabriel.png";
 
 export default function telaChat() {
   return (
@@ -12,6 +13,19 @@ export default function telaChat() {
         </div>
         <div className="nomeMensagem">
           <h1>Mensagens</h1>
+        </div>
+        <div className="containermensagensAtivas">
+          <div className="imagemPerfilAtivo">
+            <img src={gabriel} alt="" />
+          </div>
+          <div className="nomePerfilMensagem">
+            <div className="nomePerfil">
+              <p>Gabriel Anjos</p>
+            </div>
+            <div className="mensagemAntiga">
+              <p>enviou um anexo . 6h</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>

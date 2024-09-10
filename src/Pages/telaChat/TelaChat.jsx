8bcd653b59772a9d0f899122f84f9e578c6db43c
liviaBELLO <livia_bello@outlook.com>
2024-09-10 10:37:09 -0300

    primeiro container pronto

--- src/Pages/telaChat/TelaChat.jsx
+++ src/Pages/telaChat/TelaChat.jsx
@@ -2,6 +2,11 @@ import "./telaChat.css";
 import SideBar from "../../Components/SideBar/SideBar";
 import imagemPerfil from "../../assets/img/imagemPerfil.png";
 import gabriel from "../../assets/img/chatGabriel.png";
+import emerson from "../../assets/img/chatEmerson.png";
+import julia from "../../assets/img/chatJulia.png";
+import livia from "../../assets/img/chatLivia.png";
+import caua from "../../assets/img/chatCaua.png";
+import joao from "../../assets/img/chatJoao.png";
 
 export default function telaChat() {
   return (
@@ -14,20 +19,88 @@ export default function telaChat() {
         <div className="nomeMensagem">
           <h1>Mensagens</h1>
         </div>
-        <div className="containermensagensAtivas">
-          <div className="imagemPerfilAtivo">
-            <img src={gabriel} alt="" />
-          </div>
-          <div className="nomePerfilMensagem">
-            <div className="nomePerfil">
-              <p>Gabriel Anjos</p>
+        <div className="containerMensagemTodas">
+          <div className="containermensagensAtivas">
+            <div className="imagemPerfilAtivo">
+              <img src={gabriel} alt="" />
             </div>
-            <div className="mensagemAntiga">
-              <p>enviou um anexo . 6h</p>
+            <div className="nomePerfilMensagem">
+              <div className="nomePerfil">
+                <p>Gabriel Anjos</p>
+              </div>
+              <div className="mensagemRecente">
+                <p>enviou um anexo . 6h</p>
+              </div>
+            </div>
+          </div>
+          <div className="containermensagensAtivas">
+            <div className="imagemPerfilAtivo">
+              <img src={emerson} alt="" />
+            </div>
+            <div className="nomePerfilMensagem">
+              <div className="nomePerfil">
+                <p>Emerson Vieira</p>
+              </div>
+              <div className="mensagemRecente">
+                <p>enviou um anexo . 6h</p>
+              </div>
+            </div>
+          </div>
+          <div className="containermensagensAtivas">
+            <div className="imagemPerfilAtivo">
+              <img src={julia} alt="" />
+            </div>
+            <div className="nomePerfilMensagem">
+              <div className="nomePerfil">
+                <p>Julia Freitas</p>
+              </div>
+              <div className="mensagemRecente">
+                <p>Léo, e meus cinco reais?.  9h</p>
+              </div>
+            </div>
+          </div>
+          <div className="containermensagensAtivas">
+            <div className="imagemPerfilAtivo">
+              <img src={livia} alt="" />
+            </div>
+            <div className="nomePerfilMensagem">
+              <div className="nomePerfil">
+                <p>Livia Bello</p>
+              </div>
+              <div className="mensagemRecente">
+                <p>enviou um anexo . 12h</p>
+              </div>
+            </div>
+          </div>
+          <div className="containermensagensAtivas">
+            <div className="imagemPerfilAtivo">
+              <img src={caua} alt="" />
+            </div>
+            <div className="nomePerfilMensagem">
+              <div className="nomePerfil">
+                <p>Cauã Abreu</p>
+              </div>
+              <div className="mensagemRecente">
+                <p>enviou uma mensagem .  14h</p>
+              </div>
+            </div>
+          </div>
+          <div className="containermensagensAtivas">
+            <div className="imagemPerfilAtivo">
+              <img src={joao} alt="" />
+            </div>
+            <div className="nomePerfilMensagem">
+              <div className="nomePerfil">
+                <p>João Paulo</p>
+              </div>
+              <div className="mensagemRecente">
+                <p>enviou uma mensagem .  16h</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      
     </div>
   );
 }

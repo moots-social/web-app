8b20df6a1fbcc12caca2dc4a02544a06ce2b7063
liviaBELLO <livia_bello@outlook.com>
2024-09-16 11:03:34 -0300

    sideBar

--- src/Pages/telaChat/PerfilAtivoChatDireita/PerfilAtivoChatDireita.jsx
+++ src/Pages/telaChat/PerfilAtivoChatDireita/PerfilAtivoChatDireita.jsx
@@ -1,32 +1,31 @@
 import "../telaChat.css";
 import gabriel from "../../../assets/img/chatGabriel.png";
-import enviar from "../../../assets/img/iconeEnviar.png"; 
+import enviar from "../../../assets/img/iconeEnviar.png";
 
-export default function PerfilAtivoChatDireita(){
-    return(
-        <div className="containerPerfilMensagemAtual">
-        <div className="mensagemAtual">
-          <div className="imagemMensagemAtual">
-            <img src={gabriel} alt="" />
+export default function PerfilAtivoChatDireita() {
+  return (
+    <div className="containerPerfilMensagemAtual">
+      <div className="mensagemAtual">
+        <div className="imagemMensagemAtual">
+          <img src={gabriel} alt="" />
+        </div>
+        <div className="containerMensagemAtual">
+          <div className="nomeMensagemAtual">
+            <p>Gabriel Anjos</p>
           </div>
-          <div className="containerMensagemAtual">
-            <div className="nomeMensagemAtual">
-              <p>Gabriel Anjos</p>
-            </div>
-            <div className="tagUsuarioMensagemAtual">
-              <p>@gabrielAnjinho</p>
-            </div>
+          <div className="tagUsuarioMensagemAtual">
+            <p>@gabrielAnjinho</p>
           </div>
         </div>
-        <div className="containerMensagem">
-          <div className="mensagemDigitada">
+      </div>
+      <div className="containerMensagem">
+        <div className="mensagemDigitada">
           <input type="message" placeholder="mensagem"></input>
-          </div>
-          <div className="botaoEnviar">
-            <img src={enviar} alt=""></img>
-          </div>
         </div>
+        <div className="botaoEnviar">
+          <img src={enviar} alt=""></img>
         </div>
-    )
- 
+      </div>
+    </div>
+  );
 }
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,66 +1,73 @@
-import './sidebar.css'
-import pesquisa from '../../assets/img/iconePesquisa.png';
-import home from '../../assets/img/iconeHome.png';
-import mensagem from '../../assets/img/iconeMensagens.png';
-import novoPost from '../../assets/img/iconeNovoPost.png';
-import coracao from '../../assets/img/iconeCoracao.png';
-import perfil from '../../assets/img/iconePerfil.png';
-import '../../Pages/telaChat/telaChat.css';
-import '../../Pages/Salvos/Salvos.css';
+import "./sidebar.css";
+import { Link } from "react-router-dom";
+import pesquisa from "../../assets/img/iconePesquisa.png";
+import home from "../../assets/img/iconeHome.png";
+import mensagem from "../../assets/img/iconeMensagens.png";
+import novoPost from "../../assets/img/iconeNovoPost.png";
+import coracao from "../../assets/img/iconeCoracao.png";
+import perfil from "../../assets/img/iconePerfil.png";
+import "../../Pages/telaChat/telaChat.css";
+import "../../Pages/Salvos/Salvos.css";
 
-export default function SideBar(){
-    return(
-        <div className="sideBar telaChatSideBar telaSalvosSideBar">
-            <div className='containerTopo'>
-                <div className='containerInput'>
-                    <input className='pesquisa' type="text" name="" id="" />
-                </div>
-                <div className='containerIcone pesquisaIcone'>
-                    <div className='icone'>
-                        <img src={pesquisa} alt="icone-home" />
-                    </div>
-                </div>
-                <div className='containerIcone'>
-                    <div className='icone'>
-                        <img src={home} alt="icone-home" />
-                    </div>
-                    <div className='tituloIcone'>
-                        <p>Página Inicial</p>
-                    </div>
-                </div>
-                <div className='containerIcone'>
-                    <div className='icone'>
-                        <img src={mensagem} alt="icone-mensagem" />
-                    </div>
-                    <div className='tituloIcone'>
-                        <p>Mensagens</p>
-                    </div>
-                </div>
-                <div className='containerIcone'>
-                    <div className='icone'>
-                        <img src={novoPost} alt="icone-novo-post    " />
-                    </div>
-                    <div className='tituloIcone'>
-                        <p>Novo Post</p>
-                    </div>
-                </div>
-                <div className='containerIcone'>
-                    <div className='icone'>
-                        <img src={coracao} alt="icone coracao   " />
-                    </div>
-                    <div className='tituloIcone'>
-                        <p>Coleções</p>
-                    </div>
-                </div>
-            </div>
-            <div className="perfil">
-                <div className='imagemPerfil'>
-                    <img src={perfil} alt="" />
-                </div>
-                <div className='nomeUsuario'>
-                    <p>Nome do Usuário</p>
-                </div>
-            </div>
+export default function SideBar() {
+  return (
+    <div className="sideBar telaChatSideBar telaSalvosSideBar">
+      <div className="containerTopo">
+        <div className="containerInput">
+          <input className="pesquisa" type="text" name="" id="" />
         </div>
-    );
+        <div className="containerIcone pesquisaIcone">
+          <div className="icone">
+            <img src={pesquisa} alt="icone-home" />
+          </div>
+        </div>
+        <div className="containerIcone">
+          <div className="icone">
+            <img src={home} alt="icone-home" />
+          </div>
+          <div className="tituloIcone">
+            <p>Página Inicial</p>
+          </div>
+        </div>
+        <Link to="/telaChat">
+          <div className="containerIcone">
+            <div className="icone">
+              <img src={mensagem} alt="icone-mensagem" />
+            </div>
+            <div className="tituloIcone">
+              <p>Mensagens</p>
+            </div>
+          </div>
+        </Link>
+        <div className="containerIcone">
+          <div className="icone">
+            <img src={novoPost} alt="icone-novo-post    " />
+          </div>
+          <div className="tituloIcone">
+            <p>Novo Post</p>
+          </div>
+        </div>
+        <Link to="/salvos">
+          <div className="containerIcone">
+            <div className="icone">
+              <img src={coracao} alt="icone coracao   " />
+            </div>
+            <div className="tituloIcone">
+              <p>Coleções</p>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <Link to="/perfil">
+        <div className="perfil">
+          <div className="imagemPerfil">
+            <img src={perfil} alt="" />
+          </div>
+          <div className="nomeUsuario">
+            <p>Nome do Usuário</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 }

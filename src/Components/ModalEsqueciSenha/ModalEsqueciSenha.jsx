08f4d7d849f2b15d28f8c8c879307bdbd5f5789e
liviaBELLO <livia_bello@outlook.com>
2024-09-19 11:08:25 -0300

    esqueciAsenha

--- src/Components/ModalEsqueciSenha/ModalEsqueciSenha.jsx
+++ src/Components/ModalEsqueciSenha/ModalEsqueciSenha.jsx
@@ -1,14 +1,26 @@
 import "./modalEsqueciSenha.css";
+import iconeEsqueci from "../../assets/img/iconeEsqueciSenha.png";
 
-export default function modalEsqueciSenha(){
-    return(
-        <div className="containerModaLEsqueci">
-            <div className="modalEsqueci">
-                <h2></h2>
-            </div>
+export default function modalEsqueciSenha() {
+  return (
+    <div className="containerModalEsqueci bg">
+      <div className="modalEsqueci">
+        <img src={iconeEsqueci} alt=""></img>
+        <div className="tituloTextoSenha">
+          <div className="tituloSenha">
+            <h2>Dificuldades para entrar?</h2>
+          </div>
+          <div className="textoEsqueci">
+            <p>
+              Insira seu email e enviaremos um link para você voltar a acessar
+              sua conta no moots
+            </p>
+          </div>
         </div>
-
-
-
-    );
+        <div className="inputSenhaEsqueci">
+            <input className="inputEleMesmo" type="email" placeholder="Digite seu email" ></input>
+        </div>
+      </div>
+    </div>
+  );
 }

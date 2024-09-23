dc85cf016bd8b9a397176eb28d5ca1efbfaa7894
liviaBELLO <livia_bello@outlook.com>
2024-09-23 09:09:05 -0300

    sideBar Atualizada

--- src/Pages/ConfigPerfil/ConfigPerfil.jsx
+++ src/Pages/ConfigPerfil/ConfigPerfil.jsx
@@ -1,11 +1,13 @@
 import FotosPerfilECapaConfig from "./FotosPerfilECapaConfig/FotosPerfilECapaConfig";
 import ConfigAbaixoDaCapa from "./ConfigAbaixoDaCapa/ConfigAbaixoDaCapa";
+import SideBar from "../../Components/SideBar/SideBar";
 import '../../App.css'
 
 export default function ConfigPerfil() {
     return (
         <main className="mainConfig bg">
             <div>
+                <SideBar></SideBar>
                 <FotosPerfilECapaConfig></FotosPerfilECapaConfig>
                 <ConfigAbaixoDaCapa></ConfigAbaixoDaCapa>
             </div>

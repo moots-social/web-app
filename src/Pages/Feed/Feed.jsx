2c09f9e1fa02a5d0ee371f140e8cc1375e7c4abb
leoguims <leonardogmaraes7@gmail.com>
2024-09-26 09:07:52 -0300

    telaFeed Responsiva para Tablet

--- src/Pages/Feed/Feed.jsx
+++ src/Pages/Feed/Feed.jsx
@@ -2,17 +2,20 @@ import Sidebar from '../../Components/SideBar/SideBar'
 import SinoNotificacao from './SinoNotificacao/SinoNotificacao'
 import PostFeed from './PostFeed/PostFeed'
 import ConteudoFeed from './ConteudoFeed/ConteudoFeed'
+import BottomBar from '../../Components/BottomBar/BottomBar'
 import './feed.css'
 
-export default function Feed(){
-    return(
+export default function Feed() {
+    return (
         <div className='feed bg'>
             <Sidebar></Sidebar>
             <div className='containerFeed'>
                 <SinoNotificacao></SinoNotificacao>
                 <PostFeed></PostFeed>
                 <ConteudoFeed></ConteudoFeed>
+                <ConteudoFeed></ConteudoFeed>
             </div>
+            <BottomBar></BottomBar>
         </div>
     )
 }

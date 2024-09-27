4f86bade738d3201e9914530b4a9a60d2a3e27ff
liviaBELLO <livia_bello@outlook.com>
2024-09-27 11:07:07 -0300

    falta arrumar o celular

--- src/Pages/telaChat/TelaChat.jsx
+++ src/Pages/telaChat/TelaChat.jsx
@@ -3,13 +3,17 @@ import SideBarChat from "./SideBarChat/SideBarChat";
 import '../../Components/SideBar/sidebar.css'
 import PerfisChatEsquerda from"./PerfisChatEsquerda/PerfisChatEsquerda";
 import PerfilAtivoChatDireita from "./PerfilAtivoChatDireita/PerfilAtivoChatDireita";
+import BottomBar from "../../Components/BottomBar/BottomBar";
 
 export default function telaChat() {
   return (
+    <>
     <div className="divTelaChat bg">
       <SideBarChat></SideBarChat>
       <PerfisChatEsquerda></PerfisChatEsquerda>
       <PerfilAtivoChatDireita></PerfilAtivoChatDireita>
     </div>
+    <BottomBar></BottomBar>
+    </>
   );
 }

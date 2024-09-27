5fa41e12576174c8de257f83a5a8d090bcdef532
CauaSilvaChz <cauaabreusenai@gmail.com>
2024-09-27 10:43:39 -0300

    responsividade

--- src/Components/BottomBar/BottomBar.jsx
+++ src/Components/BottomBar/BottomBar.jsx
@@ -2,7 +2,6 @@ import "./bottomBar.css";
 import home from "../../assets/img/iconeHome.png";
 import mensagem from "../../assets/img/iconeMensagens.png";
 import novoPost from "../../assets/img/iconeNovoPost.png";
-import colecao from "../../assets/img/iconeCoracao.png";
 import pesquisa from "../../assets/img/iconePesquisa.png";
 import perfil from "../../assets/img/iconePerfil.png";
 
@@ -13,7 +12,6 @@ export default function BottomBar() {
         <img src={home} alt="Home"></img>
         <img src={mensagem} alt="Mensagem"></img>
         <img src={novoPost} alt="Novo Post"></img>
-        <img src={colecao} alt="Coleção"></img>
         <img src={pesquisa} className="iconePesquisa" alt="Pesquisa"></img>
         <img src={perfil} alt="Perfil"></img>
       </div>
--- src/Pages/TelaPerfil/TelaPerfil.jsx
+++ src/Pages/TelaPerfil/TelaPerfil.jsx
@@ -2,12 +2,17 @@ import SideBar from "../../Components/SideBar/SideBar"
 import './telaPerfil.css'
 import '../../App.css'
 import InformacoesDoPerfil from "./InformacoesDoPerfil/InformacoesDoPerfil"
+import BottomBar from "../../Components/BottomBar/BottomBar"
 
 export default function TelaPerfil() {
     return (
-        <main className="bg mainPerfil">
+        <>
+        <main className="bg mainPerfil">    
             <SideBar></SideBar>
             <InformacoesDoPerfil></InformacoesDoPerfil>
         </main>
+        <BottomBar></BottomBar>
+        </>
+
     )
 }

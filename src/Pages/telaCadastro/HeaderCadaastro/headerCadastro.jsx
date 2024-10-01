23c0ec395e1569e98e3b45eb7fe28f4783f06313
CauaSilvaChz <cauaabreusenai@gmail.com>
2024-10-01 10:54:01 -0300

    completo

--- src/Pages/telaCadastro/HeaderCadaastro/headerCadastro.jsx
+++ src/Pages/telaCadastro/HeaderCadaastro/headerCadastro.jsx
@@ -1,11 +1,15 @@
 import '../telaCadastro.css'
 import logo from '../../../assets/img/Logo.png'
+import logoSemTexto from '../../../assets/img/iconeLogoSemTexto.png'
 
 export default function HeaderCadastro(){
     return(
         <header className='headerCadastro'>
             <div className="logo">
                 <img src={logo} alt="" />
+            </div>
+            <div className="logoSemTexto">
+                <img src={logoSemTexto} alt="" />
             </div>
         </header>
     )

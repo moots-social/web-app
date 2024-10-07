a181c7ceda8d3fa5493cf347299b27ced08a2deb
Wickedju <almeida_2010@outlook.com>
2024-10-07 08:45:14 -0300

    responsividade duas telas salvos e inicial

---
+++ src/Components/BottomBar/BottomBar.jsx
@@ -0,0 +1,19 @@
+import "./bottomBar.css";
+import home from "../../assets/img/iconeHome.png";
+import mensagem from "../../assets/img/iconeMensagens.png";
+import novoPost from "../../assets/img/iconeNovoPost.png";
+import pesquisa from "../../assets/img/iconePesquisa.png";
+import perfil from "../../assets/img/iconePerfil.png";
+
+
+export default function BottomBar() {
+  return (
+      <div  className="containerBottomBar">
+        <img src={home} alt="Home"></img>
+        <img src={mensagem} alt="Mensagem"></img>
+        <img src={novoPost} alt="Novo Post"></img>
+        <img src={pesquisa} className="iconePesquisa" alt="Pesquisa"></img>
+        <img src={perfil} alt="Perfil"></img>
+      </div>
+  );
+}
---
+++ src/Pages/Principal/BotaoAtivo/BotaoSobre.jsx
@@ -0,0 +1,12 @@
+import "../../../Components/Header/BotaoHeader/button.css";
+import "../../../Components/Responsividade/Responsividade.css"
+
+function BotaoSobre() {
+  return (
+    <div>
+      <button className="btn btnSobre">Sobre</button>
+    </div>
+  );
+}
+
+export default BotaoSobre;
